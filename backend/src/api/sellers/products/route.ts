d13fb979491a8737f030070a8acb9a5ad4d4150d
0xdfinity <130572243+0xdfinity@users.noreply.github.com>
2026-07-28 18:01:14 +0100

    feat(backend): seller product creation and listing with ownership links

---
+++ backend/src/api/sellers/products/route.ts
@@ -0,0 +1,43 @@
+import {
+  AuthenticatedMedusaRequest,
+  MedusaResponse,
+} from "@medusajs/framework/http"
+import { HttpTypes } from "@medusajs/framework/types"
+import { ContainerRegistrationKeys } from "@medusajs/framework/utils"
+import createSellerProductWorkflow from "../../../workflows/marketplace/create-seller-product"
+
+export const POST = async (
+  req: AuthenticatedMedusaRequest<HttpTypes.AdminCreateProduct>,
+  res: MedusaResponse
+) => {
+  const { result } = await createSellerProductWorkflow(req.scope)
+    .run({
+      input: {
+        seller_admin_id: req.auth_context.actor_id,
+        product: req.validatedBody,
+      },
+    })
+
+  res.json({
+    product: result.product,
+  })
+}
+
+export const GET = async (
+  req: AuthenticatedMedusaRequest,
+  res: MedusaResponse
+) => {
+  const query = req.scope.resolve(ContainerRegistrationKeys.QUERY)
+
+  const { data: [sellerAdmin] } = await query.graph({
+    entity: "seller_admin",
+    fields: ["seller.products.*"],
+    filters: {
+      id: [req.auth_context.actor_id],
+    },
+  })
+
+  res.json({
+    products: sellerAdmin.seller.products,
+  })
+}
---
+++ backend/src/workflows/marketplace/create-seller-product/index.ts
@@ -0,0 +1,91 @@
+import { CreateProductWorkflowInputDTO } from "@medusajs/framework/types"
+import {
+  createWorkflow,
+  transform,
+  WorkflowResponse,
+} from "@medusajs/framework/workflows-sdk"
+import {
+  createProductsWorkflow,
+  CreateProductsWorkflowInput,
+  createRemoteLinkStep,
+  useQueryGraphStep,
+} from "@medusajs/medusa/core-flows"
+import { MARKETPLACE_MODULE } from "../../../modules/marketplace"
+import { Modules } from "@medusajs/framework/utils"
+
+type WorkflowInput = {
+  seller_admin_id: string
+  product: CreateProductWorkflowInputDTO
+}
+
+const createSellerProductWorkflow = createWorkflow(
+  "create-seller-product",
+  (input: WorkflowInput) => {
+    // make the product available in the store's default sales channel
+    const { data: stores } = useQueryGraphStep({
+      entity: "store",
+      fields: ["default_sales_channel_id"],
+    })
+
+    const productData = transform({
+      input,
+      stores,
+    }, (data) => {
+      return {
+        products: [{
+          ...data.input.product,
+          sales_channels: [
+            {
+              id: data.stores[0].default_sales_channel_id,
+            },
+          ],
+        }],
+      }
+    })
+
+    const createdProducts = createProductsWorkflow.runAsStep({
+      input: productData as CreateProductsWorkflowInput,
+    })
+
+    const { data: sellerAdmins } = useQueryGraphStep({
+      entity: "seller_admin",
+      fields: ["seller.id"],
+      filters: {
+        id: input.seller_admin_id,
+      },
+    }).config({ name: "retrieve-seller-admins" })
+
+    const linksToCreate = transform({
+      input,
+      createdProducts,
+      sellerAdmins,
+    }, (data) => {
+      return data.createdProducts.map((product) => {
+        return {
+          [MARKETPLACE_MODULE]: {
+            seller_id: data.sellerAdmins[0].seller.id,
+          },
+          [Modules.PRODUCT]: {
+            product_id: product.id,
+          },
+        }
+      })
+    })
+
+    createRemoteLinkStep(linksToCreate)
+
+    const { data: products } = useQueryGraphStep({
+      entity: "product",
+      fields: ["*", "variants.*"],
+      filters: {
+        id: createdProducts[0].id,
+      },
+    }).config({ name: "retrieve-products" })
+
+    return new WorkflowResponse({
+      product: products[0],
+    })
+  }
+)
+
+export default createSellerProductWorkflow
